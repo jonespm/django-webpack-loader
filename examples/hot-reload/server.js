e6e12720ac7a662c438e3a3593ed11b28bc73fc8
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2021-05-27 15:55:02 -0400

    Adding Dockerfile Updating server to listen on all interfaces

--- examples/hot-reload/server.js
+++ examples/hot-reload/server.js
@@ -15,6 +15,6 @@ webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
 
-server.listen(3000, 'localhost', () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('dev server listening on port 3000');
 });
